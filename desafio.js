4f996c4bbbd3ce3085772603af63b0817b5b333c
joaossmacedo <joaossmacedo@gmail.com>
2019-01-10 12:18:34 -0300

    remove table when showing more info and goes back to initial table

--- desafio.js
+++ desafio.js
@@ -1,23 +1,25 @@
+// guarda os valores dos individuos fornecidos pela API
 var roster = [];
 
-// pega os valores dados pela API e cria uma table(Figura 1)
+// pega os valores dados pela API e cria a table(Figura 1)
 $.ajax({
   url: 'https://randomuser.me/api/?results=25&noinfo',
   dataType: 'json',
   success: function(data) {
     roster = data.results;
-    // cria a table e a sua head
-    $("body .container").append("<table><thead><tr><th>Nome</th><th>Email</th><th>Telefone</th></tr></thead></table>")
-    console.log(roster);
+    // cria a table e a sua thead
+    $("body .container").append('<table id="tbl"><thead><tr><th>Nome</th><th>Email</th><th>Telefone</th></tr></thead></table>');
+
     // popula a table
-    populate_table();
+    populateTable();
   }
 });
 
-// constroi a table usando os valores fornecidos pela API
-function populate_table(){
+// constroi a table usando os valores fornecidos pela API(guardada no roster)
+function populateTable(){
   $("table").append("<tbody></tbody>")
 
+  // coloca os valores na table
   for (i = 0; i < 25; i++) {
     var name = roster[i].name.first;
     var email = roster[i].email;
@@ -26,16 +28,34 @@ function populate_table(){
   }
 }
 
-// permite ao clicar em um tr, criar uma nova pagina em que sao mostrados mais dados(Figura 2)
+// deleta a table
+function deleteTable() {
+  table = document.getElementById('tbl');
+  // o uso do index e necessario visto que getElementsByClassName retorna HTMLCollection
+  doc = document.getElementsByClassName('container')[0];
+  doc.removeChild(table);
+}
+
+// reconstroi a table
+function createTable() {
+  $("body .container").append('<table id="tbl"><thead><tr><th>Nome</th><th>Email</th><th>Telefone</th></tr></thead></table>');
+  populateTable();
+}
+
+// permite ao clicar em um tr, mostrar mais dados(Figura 2)
 $('body').on('click','tr', function() {
   var all_tr = $('table tr');
+  // acha a posicao do tr clicado em relacao aos outros tr
   for (var i = 0; i < all_tr.length; i++) {
     if(all_tr[i] == this){
       break;
     }
   }
+
   var pos = i - 1
+  // checa se a posicao e maior que -1 para nao contar com o thead
   if(pos > -1){
+    // recebe nome, foto, email, telefone, aniversario e endereco
     var photo = roster[pos].picture.medium;
     var name = roster[pos].name.first + ' ' + roster[pos].name.last;
     var email = roster[pos].email;
@@ -43,25 +63,24 @@ $('body').on('click','tr', function() {
     var phone = roster[pos].phone;
     var adress = roster[pos].location.street;
 
-    console.log(photo);
-    console.log(name);
-    console.log(email);
-    console.log(bday);
-    console.log(phone);
-    console.log(adress);
-
-    // <div class="grid-container">
-    // <div class="grid-item">1</div>
-    // <div class="grid-item">2</div>
-    // <div class="grid-item">3</div>
-    // </div>
-
     // adiciona nome, foto, email, telefone, aniversario e endereco
-    $("body .container").append('<h3>' + name +
+    $("body .container").append('<div id=info><h3 id=name>' + name +
     '</h3><div class="grid"><div><img src="' + photo +
     '"></div><div><p>' + email + '</p><p>' + phone + '</p></div><div><p>' +
-    bday + '</p><p>' + adress + '</p></div></div>')
+    bday + '</p><p>' + adress + '</p></div></div></div>');
 
-    console.log(pos);
+    // deleta a table
+    deleteTable();
+
   }
+});
+
+// ao clicar no h3 retorna para a pagina inicial(com a table)(Figura 1)
+$('body').on('click','#name', function() {
+  info = document.getElementById('info');
+  // o uso do index e necessario visto que getElementsByClassName retorna HTMLCollection
+  doc = document.getElementsByClassName('container')[0];
+  doc.removeChild(info);
+
+  createTable();
 });
